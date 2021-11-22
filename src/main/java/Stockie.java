e4408ba0c87bba71fe6c941f43ff07d02fce1e9b
Oliver Isaak <oliver.9002s@gmail.com>
2021-11-22 12:46:32 +0100

    DatabaseConnector

--- src/main/java/Stockie.java
+++ src/main/java/Stockie.java
@@ -8,5 +8,7 @@ public class Stockie {
     public static void main(String[] args){
         System.out.println("test");
         Candle mycandle = new Candle();
+        DatabaseConnector db = new DatabaseConnector();
+
     }
 }
