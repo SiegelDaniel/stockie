6d1ce2aee80626c9e96f7f6108ac8527aa4169d1
Oliver Isaak <oliver.9002s@gmail.com>
2021-11-22 12:47:44 +0100

    DatabaseConnector

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,4 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
-//hahsdjsdkasjhdkjashdjkashd Schwa
-    //test2
-}
+    public DatabaseConnector(){
+        String url = "jdbc:mysql://185.188.250.67:3306";
+        String user = "root";
+        String pass = "StefanWolf2k21";
+        // Verbindung aufbauen
+        try {
+            Connection con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Verbindung erfolgreich hergestellt");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    }
+
+
+
