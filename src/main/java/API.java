4ddef3d28c3e3f99f15dc4c459c7841ec204abde
Oliver Isaak <oliver.9002s@gmail.com>
2021-12-02 19:03:00 +0100

    APICall hinzugefügt URL gekapselt

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -4,10 +4,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.mysql.cj.xdevapi.JsonArray;
+import com.mysql.cj.xdevapi.JsonParser;
+
 import java.io.IOException;
 public class API {
 
@@ -18,6 +22,10 @@ public class API {
         HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
         HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
         String response = request.execute().parseAsString();
+
+     //   JsonArray liste = new JsonArray();
+
+
         System.out.println(response);
         return objectMapper.readValue(response, JsonNode.class);
     }
