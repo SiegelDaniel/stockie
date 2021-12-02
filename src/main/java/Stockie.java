163d812e4f05341f3b4146d4982bb60d8e18e969
Desktop-Jannik <jannik_walter@online.de>
2021-12-02 19:01:56 +0100

    issues

--- src/main/java/Stockie.java
+++ src/main/java/Stockie.java
@@ -22,6 +22,8 @@ public class Stockie {
         URLModel url = new URLModel("1A79MCHMT69G16RE","AAPL","5min","TIME_SERIES_INTRADAY");
         API api = new API();
         String alphaVantageUrl = url.getUrl();
-        api.getWebPage(alphaVantageUrl);
+        api.getWebPage(alphaVantageUrl, "3. low");
+        api.getWebPage(alphaVantageUrl, "1. open");
+
     }
 }
