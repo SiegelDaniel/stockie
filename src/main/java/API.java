65d854b36d4577b066b0ef0d12a64760abd8ff60
Desktop-Jannik <jannik_walter@online.de>
2021-12-02 18:16:48 +0100

    Adrian: added json parser

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -9,6 +9,8 @@ import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
+import java.util.Iterator;
+
 import org.json.*;
 public class API {
 
@@ -19,7 +21,34 @@ public class API {
         HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
         HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
         String response = request.execute().parseAsString();
-        System.out.println(response);
+
+        try {
+            JSONObject obj = new JSONObject(response);
+
+            String mData = obj.getJSONObject("Time Series (5min)").toString();
+
+            JSONObject objtwo = new JSONObject(mData.trim());
+
+            Iterator<?> keys = objtwo.keys();
+
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                System.out.println(objtwo.getJSONObject(key).getString("3. low"));
+                System.out.println("\n");
+            }
+
+
+
+
+
+
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+
+
+        //System.out.println(response);
         return objectMapper.readValue(response, JsonNode.class);
     }
 
